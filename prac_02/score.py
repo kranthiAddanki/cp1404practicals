122b2dbc46d27dd49e304cad4bda5c2e624a78a9
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2024-09-27 11:37:13 +1000

    Add version_2 to randomise score

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,8 +4,31 @@ score program
 """
 
 
+# def main():
+#     score = float(input("Enter score: "))
+#     print(calculate_result(score))
+#
+# def calculate_result(score):
+#     if score < 0 or score > 100:
+#         return "Invalid score"
+#     elif score >= 90:
+#         return "Excellent"
+#     elif score >= 50:
+#         return "pass"
+#     else:
+#         return "Bad"
+
+
+# main()
+
+# Version_2
+
+import random
+
 def main():
-    score = float(input("Enter score: "))
+# randomise score
+    score = random.randint(1,100)
+    print(score)
     print(calculate_result(score))
 
 def calculate_result(score):
