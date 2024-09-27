011b9d5b4b43c79f741e4f940e8ef1b6f3b888bc
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2024-09-27 11:30:26 +1000

    Fix program to calculate score with function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,29 +1,22 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404/CP5632 - Practical 2
+score program
 """
 
 
+def main():
+    score = float(input("Enter score: "))
+    print(calculate_result(score))
 
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#     print("Excellent")
-# if score < 50:
-#     print("Bad")
+def calculate_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "pass"
+    else:
+        return "Bad"
 
-score = float(input("Enter score: "))
-if score < 0 or score> 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("pass")
-else :
-    print("Bad")
+
+main()
