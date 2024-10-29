3c2ecabfb9ec73bdddafdf3d84957a91cf7cb194
kranthiAddanki <kranthi.addanki@my.jcu.edu.au>
2024-10-29 23:24:34 +1000

    Complete class for programming_languages program

---
+++ prac_06/programming_language.py
@@ -0,0 +1,21 @@
+# 15 minutes
+class ProgrammingLanguage:
+    """Represents information about programming languages."""
+
+
+    def __init__(self, field, typing, reflection, year):
+        """Construct a programming language using given values."""
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+    def is_dynamic(self):
+        """Check if the language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+
+    def __str__(self):
+        """Return string representation """
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
